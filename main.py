dde9fdf845c09e7b6cf8b75d9087441feefe25c4
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2018-03-15 04:46:26 +0900

    dict test4

--- main.py
+++ main.py
@@ -20,14 +20,20 @@ def LocationChk(location):
 def Iswaiting(delay_info):
     return " 회차지 대기중 " if delay_info is "N" else ""
 
+
 def MakeFoodList(food_menus, day):
-    return "[{0} 메뉴] ".format(food_menus[0][day]) + "\n" + "<한식>\n" + food_menus[1]["korean"][day] + "\n" + "<일품1>\n" + food_menus[1]["food1"][day] + "\n" + "<일품2>\n" + food_menus[1]["food2"][day] + "\n\n\n"
+    return "[{0} 메뉴] ".format(food_menus[0][day]) + "\n" + "<한식>\n" + food_menus[1]["korean"][day] + "\n" + "<일품1>\n" + food_menus[1]["food1"][day] + "\n" + "<일품2>\n" + \
+           food_menus[1]["food2"][day] + "\n\n\n"
 
 
 button_data = {
-    "type": "buttons",
-    "buttons": ["시작하기", "도움말", "* 셔틀버스 시간표 * ", "* 033 *", "* 033-2 *", "* 054 *", "* 054-2 *",  "* 학식 메뉴 *", "* 건의 사항 *"]
+    "Keyboard": {
+        "type": "buttons",
+        "buttons": ["시작하기", "도움말", "* 셔틀버스 시간표 * ", "* 033 *", "* 033-2 *", "* 054 *", "* 054-2 *", "* 학식 메뉴 *", "* 건의 사항 *"]
+    }
 }
+
+
 @app.route('/keyboard')
 def Keyboard():
     send_data = {
@@ -36,7 +42,6 @@ def Keyboard():
             "buttons": ["시작하기", "도움말"]
         }
     }
-
     return jsonify(button_data)
 
 
@@ -96,10 +101,10 @@ def Message():
             send_data = {
                 "message": {
                     "text": "[중부대학교] -> [원흥역] \n* 첫차 시간 : " + bus_info[3][0] + "\n* 막차 시간 : " + bus_info[3][1] + "\n" +
-                            "[원흥역] -> [중부대학교] \n* 첫차 시간 : " + bus_info[1][0] + "\n* 막차 시간 : " +bus_info[1][1] + "\n\n" +
+                            "[원흥역] -> [중부대학교] \n* 첫차 시간 : " + bus_info[1][0] + "\n* 막차 시간 : " + bus_info[1][1] + "\n\n" +
                             "[중부대학교] -> [원흥역] :\n{0}{1}{2}".format(TimeChk(bus_info[0][2]), LocationChk(bus_info[0][6]), Iswaiting(bus_info[0][4])) + "\n\n\n" +
                             "[원흥연합기숙사] -> [중부대학교] :\n{0}{1}{2}".format(TimeChk(bus_info[1][2]), LocationChk(bus_info[1][6]), Iswaiting(bus_info[1][4])) + "\n\n\n" +
-                            "[고양동 시장] -> [중부대학교] :\n{0}{1}{2}".format(TimeChk(bus_info[2][2]),LocationChk(bus_info[2][6]),Iswaiting(bus_info[2][4])) + "\n\n\n" +
+                            "[고양동 시장] -> [중부대학교] :\n{0}{1}{2}".format(TimeChk(bus_info[2][2]), LocationChk(bus_info[2][6]), Iswaiting(bus_info[2][4])) + "\n\n\n" +
                             "[고양동 시장] -> [원흥역] :\n{0}{1}{2}".format(TimeChk(bus_info[3][2]), LocationChk(bus_info[3][6]), Iswaiting(bus_info[3][4])) + "\n\n\n"
                 }
             }
@@ -107,10 +112,10 @@ def Message():
             send_data = {
                 "message": {
                     "text": "[중부대학교] -> [원흥역] \n* 첫차 시간 : " + bus_info[3][0] + "\n* 막차 시간 : " + bus_info[3][1] + "\n" +
-                            "[원흥역] -> [중부대학교] \n* 첫차 시간 : " + bus_info[1][0] + "\n* 막차 시간 : " +bus_info[1][1] + "\n\n" +
+                            "[원흥역] -> [중부대학교] \n* 첫차 시간 : " + bus_info[1][0] + "\n* 막차 시간 : " + bus_info[1][1] + "\n\n" +
                             "[중부대학교] -> [원흥역] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[0][3]), LocationChk(bus_info[0][7]), Iswaiting(bus_info[0][5])) + "\n\n\n" +
                             "[원흥연합기숙사] -> [중부대학교] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[1][3]), LocationChk(bus_info[1][7]), Iswaiting(bus_info[1][5])) + "\n\n\n" +
-                            "[고양동 시장] -> [중부대학교] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[2][3]),LocationChk(bus_info[2][7]),Iswaiting(bus_info[2][5])) + "\n\n\n" +
+                            "[고양동 시장] -> [중부대학교] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[2][3]), LocationChk(bus_info[2][7]), Iswaiting(bus_info[2][5])) + "\n\n\n" +
                             "[고양동 시장] -> [원흥역] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[3][3]), LocationChk(bus_info[3][7]), Iswaiting(bus_info[3][5])) + "\n\n\n"
                 }
             }
@@ -125,9 +130,9 @@ def Message():
                 "message": {
                     "text": "[고양동시장] -> [필리핀 참전비] \n* 첫차 시간 : " + bus_info[0][0] + "\n* 막차 시간 : " + bus_info[0][1] + "\n" +
                             "[필리핀 참전비] -> [고양동 시장] \n* 첫차 시간 : " + bus_info[3][0] + "\n* 막차 시간 : " + bus_info[3][1] + "\n" +
-                            "[고양동시장] -> [필리핀 참전비] :\n{0}{1}{2}".format(TimeChk(bus_info[0][2]), LocationChk(bus_info[0][6]),Iswaiting(bus_info[0][4])) + "\n\n\n" +
+                            "[고양동시장] -> [필리핀 참전비] :\n{0}{1}{2}".format(TimeChk(bus_info[0][2]), LocationChk(bus_info[0][6]), Iswaiting(bus_info[0][4])) + "\n\n\n" +
                             "[중부대학교] -> [필리핀 참전비] :\n{0}{1}{2}".format(TimeChk(bus_info[1][2]), LocationChk(bus_info[1][6]), Iswaiting(bus_info[1][4])) + "\n\n\n" +
-                            "[중부대학교] -> [고양동 시장] :\n{0}{1}{2}".format(TimeChk(bus_info[2][2]),LocationChk(bus_info[2][6]),Iswaiting(bus_info[2][4])) + "\n\n\n" +
+                            "[중부대학교] -> [고양동 시장] :\n{0}{1}{2}".format(TimeChk(bus_info[2][2]), LocationChk(bus_info[2][6]), Iswaiting(bus_info[2][4])) + "\n\n\n" +
                             "[관산동 삼거리]-> [고양동 시장(중부대학교)] :\n{0}{1}{2}".format(TimeChk(bus_info[3][2]), LocationChk(bus_info[3][6]), Iswaiting(bus_info[3][4])) + "\n\n\n"
                 }
             }
@@ -136,9 +141,9 @@ def Message():
                 "message": {
                     "text": "[고양동시장] -> [필리핀 참전비] \n* 첫차 시간 : " + bus_info[0][0] + "\n* 막차 시간 : " + bus_info[0][1] + "\n" +
                             "[필리핀 참전비] -> [고양동 시장] \n* 첫차 시간 : " + bus_info[3][0] + "\n* 막차 시간 : " + bus_info[3][1] + "\n" +
-                            "[고양동시장] -> [필리핀 참전비] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[0][3]), LocationChk(bus_info[0][7]),Iswaiting(bus_info[0][5])) + "\n\n\n" +
+                            "[고양동시장] -> [필리핀 참전비] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[0][3]), LocationChk(bus_info[0][7]), Iswaiting(bus_info[0][5])) + "\n\n\n" +
                             "[중부대학교] -> [필리핀 참전비] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[1][3]), LocationChk(bus_info[1][7]), Iswaiting(bus_info[1][5])) + "\n\n\n" +
-                            "[중부대학교] -> [고양동 시장] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[2][3]),LocationChk(bus_info[2][7]),Iswaiting(bus_info[2][5])) + "\n\n\n" +
+                            "[중부대학교] -> [고양동 시장] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[2][3]), LocationChk(bus_info[2][7]), Iswaiting(bus_info[2][5])) + "\n\n\n" +
                             "[관산동 삼거리]-> [고양동 시장(중부대학교)] 2번째 :\n{0}{1}{2}".format(TimeChk(bus_info[3][3]), LocationChk(bus_info[3][7]), Iswaiting(bus_info[3][5])) + "\n\n\n"
                 }
             }
@@ -166,8 +171,7 @@ def Message():
                         "\"도움말\"을 입력하여 가능한 도움말을 찾아보세요! "
             }
         }
-    send_data.update(button_data)
-    print(send_data)
+    send_data.update(button_data)  # merge message and keyboard menu
     return jsonify(send_data)
 
 
